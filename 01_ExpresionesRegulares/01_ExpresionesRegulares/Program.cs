8d926b8b9f0968994fbd4325f0cdeb3ef286f656
Moises Alonso <maalonso@uvg.edu.gt>
2023-01-31 23:22:16 -0600

    Acepta el usuario del correo

---
+++ 01_ExpresionesRegulares/01_ExpresionesRegulares/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01_ExpresionesRegulares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string opt = "";
+            while (!opt.Equals("0")) {
+                Console.WriteLine("Ingrese un correo electrónico, o 0 si desea salir");
+                opt = Console.ReadLine();
+
+                if (!opt.Equals("0")) {
+                    if (Regex.IsMatch(opt, "^(([a-zA-Z0-9]|_|\\.)+)$"))
+                    {
+                        Console.WriteLine("Si es un correo electronico");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No es un correo electronico");
+                    }
+                }
+                
+            }
+            
+        }
+    }
+}
